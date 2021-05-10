fbc651fea27ae0bc9ff4bfdf49cc28ecc54502c3
ahana2021 <81161507+ahana2021@users.noreply.github.com>
2021-05-10 16:40:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+https://teachablemachine.withgoogle.com/models/yfQ8cIUg2/
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format='png',
+    png_quality=95,
+});
+camera = document.getElementById("camera");
+Webcam.attach( '#camera' );
